41995fa77c2343fb9c9039758215312f174f3bf0
Gabriel Alves <127686273+GabrielAlves263@users.noreply.github.com>
2023-09-21 21:37:26 -0300

    Update Main.java

--- Lista2/src/lista2/Main.java
+++ Lista2/src/lista2/Main.java
@@ -3,21 +3,6 @@ package lista2;
 public class Main {
 
 	public static void main(String[] args) {
-		Utilidades utils = new Utilidades();
-		
-		utils.imprimirElementosF(6);
-		int[] vet2 = {4, 4, 5, 2, 4, 1};
-		
-		
-//		boolean test = utils.verificarNumeroPerfeito(858986905);
-		
-		
-//		for(int i = 0; i < vet1.length; i++) {
-//			for(int j = 0; j < vet1.length; j++) {
-//				System.out.print(vet1[i][j] + "\t");
-//			}
-//			System.out.println("\n");
-//		}
+		Utilidades utils = new Utilidades()
 	}
-
 }
